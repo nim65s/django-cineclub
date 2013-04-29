848fc1626c14800d11612bbd8043f0a2f4ae5a42
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-04-29 22:07:28 +0200

    ajout de fichiers manquants…

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url
+from cine.views import *
+
+
+urlpatterns = patterns('',
+    url(r'^$', previsions, name='previsions'),
+    url(r'^films$', films, name='films'),
+    url(r'^dispos$', dispos, name='dispos'),
+    url(r'^votes$', votes, name='votes'),
+    url(r'^cinephiles$', cinephiles, name='cinephiles'),
+    url(r'^profil$', profil, name='profil'),
+    url(r'^faq$', faq, name='faq'),
+    #url(r'^about$', 'about'),
+
+    url(r'^comms/(?P<slug>[^/]+)$', comms, name='comms'),
+)
