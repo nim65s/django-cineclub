f8b626c5699ddced0f8120e50983a98c7fd9e6e8
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-04-29 23:33:00 +0200

    passage de profil de cine à perso, again

--- models.py
+++ models.py
@@ -85,25 +85,6 @@ class FilmForm(ModelForm):
         exclude = ('respo', 'slug', 'vu')
 
 
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        exclude = ('password', 'is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')
-
-    def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        instance = getattr(self, 'instance', None)
-        if instance and instance.id:
-            self.fields['last_login'].widget.attrs['readonly'] = True
-            self.fields['date_joined'].widget.attrs['readonly'] = True
-
-    def clean_last_login(self):
-        return self.instance.last_login
-
-    def clean_date_joined(self):
-        return self.instance.date_joined
-
-
 class Vote(Model):
     film = ForeignKey(Film)
     cinephile = ForeignKey(User)
