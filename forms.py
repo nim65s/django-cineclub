035313327ef5e1806c89282d8ed16c8a2f71843e
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-10-13 15:37:37 +0200

    forms.py…

---
+++ forms.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+
+from django.forms import ModelForm
+
+from .models import Film
+
+
+class FilmForm(ModelForm):
+    class Meta:
+        model = Film
+        exclude = ('respo', 'slug', 'vu')
