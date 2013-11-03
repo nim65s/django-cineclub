d8367ebd5106f5d87a8edb01a135a8cc412b438d
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-11-03 17:39:25 +0100

    film_url se base sur url_get

--- templatetags/cine_extra.py
+++ templatetags/cine_extra.py
@@ -2,12 +2,11 @@
 
 from django import template
 from django.core.urlresolvers import reverse
+from perso.templatetags.perso_extra import url_get
 
 register = template.Library()
 
 
 @register.simple_tag
 def films_url(request, key, value):
-    get = request.GET.copy()
-    get[key] = value
-    return "%s?%s" % (reverse('cine:films'), get.urlencode())
+    return "%s%s" % (reverse('cine:films'), url_get(request, key, value))
