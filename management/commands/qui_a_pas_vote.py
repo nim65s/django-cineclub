f6a7f6bcad5245c498833fe6f1209a7c44e579f5
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-09-15 19:13:47 +0200

    typo

--- management/commands/qui_a_pas_vote.py
+++ management/commands/qui_a_pas_vote.py
@@ -7,6 +7,6 @@ class Command(BaseCommand):
     help = 'Affiche les gens qui ont pas voté (c’est pas bien ! :P)'
 
     def handle(self, *args, **options):
-        dispos = Soiree.objects.a_venir.first().dispotowatch_set.filter(dispo='O').values_list('cinephile', flat=True)
+        dispos = Soiree.objects.a_venir().first().dispotowatch_set.filter(dispo='O').values_list('cinephile', flat=True)
         for vote in Vote.objects.filter(choix=9999, film__vu=False, cinephile__in=dispos):
             print('%s n’a pas voté pour %s' % (vote.cinephile, vote.film))
