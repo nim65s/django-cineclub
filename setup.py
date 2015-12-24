1d70e8e153ae29690c809b3059cbb656e1cbc291
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-24 19:04:29 +0100

    requirements: requests

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         install_requires=[
             'Django',
             'django-bootstrap3',
+            'requests',
             ],
         include_package_data=True,
         license='GPL License',
