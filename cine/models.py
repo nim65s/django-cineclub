fa5530493febafbcb4027c8272e3c55a3c49f7e5
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-01-10 21:29:01 +0100

    send mail to all cinephiles…

--- cine/models.py
+++ cine/models.py
@@ -152,7 +152,7 @@ class Soiree(Model):
                 'lien': full_url(reverse('cine:dtw', args=(self.pk, 1)))}
             text, html = (get_template('cine/mail.%s' % alt).render(ctx) for alt in ['txt', 'html'])
             msg = EmailMultiAlternatives('Soirée Ajoutée !', text, settings.DEFAULT_FROM_EMAIL,
-                    [cinephile.user.email for cinephile in self.cinephile_set.all()])
+                    [cinephile.user.email for cinephile in Cinephile.objects.all()])
             msg.attach_alternative(html, 'text/html')
             if not settings.DEBUG:
                 msg.send()
