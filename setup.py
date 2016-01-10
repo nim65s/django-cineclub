04d7eeef5e268b69d24691807cfac199ca0f4b80
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-01-10 22:35:58 +0100

    This is v2.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
         name='django-cineclub',
-        version='2.0.1',
+        version='2.1.0',
         packages=['cine'],
         install_requires=[
             'Django',
